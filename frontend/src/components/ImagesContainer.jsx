98d2370d141875c84a6965f22085ab038ffe369f
Eden Kirin <eden@ekirin.com>
2022-09-16 17:37:51 +0200

    Restructure layout styles

--- frontend/src/components/ImagesContainer.jsx
+++ frontend/src/components/ImagesContainer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
+import Typography from "@mui/material/Typography";
 
 const IMAGE_PLACEHOLDER = "/img/dummy-square.jpeg";
 
@@ -9,22 +10,25 @@ const ImageCard = (props) => {
     const imageInfo = props.imageInfo;
     const imgId = `${props.idPrefix || ""}${imageInfo.elementId}`;
     let imgSrc;
+    let imgClass = "qr-img";
 
     switch (props.source) {
         case "source":
             imgSrc = imageInfo.srcImage.src;
+            imgClass += " qr-source-img";
             break;
         case "processed":
             imgSrc = imageInfo.processedImage || IMAGE_PLACEHOLDER;
+            imgClass += " qr-processed-img";
             break;
         default:
             imgSrc = IMAGE_PLACEHOLDER;
     }
 
     return (
-        <ImageListItem key={imageInfo.fileName}>
-            <img src={imgSrc} alt={imageInfo.fileName} id={imgId} />
-            <ImageListItemBar title={imageInfo.fileName} position="below" sx={{ textAlign: "center" }} />
+        <ImageListItem key={imageInfo.fileName} className="qr-img-list-item">
+            <img src={imgSrc} className={imgClass} alt={imageInfo.fileName} id={imgId} />
+            <Typography className="qr-img-title">{imageInfo.fileName}</Typography>
         </ImageListItem>
     );
 };
@@ -38,5 +42,9 @@ export default function ImagesContainer(props) {
         ));
     }
 
-    return <ImageList cols={10}>{imageCards}</ImageList>;
+    return (
+        <ImageList cols={10} className="qr-img-list">
+            {imageCards}
+        </ImageList>
+    );
 }
